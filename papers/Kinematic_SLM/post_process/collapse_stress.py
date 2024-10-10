found a bug in stress calculation for slope stability and rolled back to geometrical mdoel being the default

papers/Kinematic_SLM/post_process/collapse_stress.py
@@ -6,7 +6,7 @@ from matplotlib import colormaps
 import matplotlib.cm as cm
 import matplotlib.colors as colors
 from void_migration.params import load_file
-from void_migration import stress
+from void_migration import stress, operators
 
 plt.style.use("papers/Kinematic_SLM/paper.mplstyle")
 
@@ -29,7 +29,7 @@ cmap.set_bad("w", 0.0)
 
 max_angle = 60
 
-fig = plt.figure(figsize=[3.31894680556, 2.2])
+fig = plt.figure(figsize=[3.31894680556, 2.6], constrained_layout=True)
 ax = fig.subplots(3, 2)
 
 files = glob(f"output/collapse_stress/data/s_*.npy")
@@ -49,58 +49,77 @@ last_swap_f = np.load(files[-1])
 
 labelpad = 2
 
-p.stress_mode = "active"
-
 for i in [0, 1]:
     if i == 0:
         sigma = stress.calculate_stress(initial, last_swap_i, p)
+        nu = operators.get_solid_fraction(initial)
     else:
         sigma = stress.calculate_stress(final, last_swap_f, p)
+        nu = operators.get_solid_fraction(final)
     pressure = stress.get_pressure(sigma, p, last_swap_i)
     deviatoric = stress.get_deviatoric(sigma, p, last_swap_i)
     friction_angle = stress.get_friction_angle(sigma, p, last_swap_i)
+    sigma_xx = stress.get_sigma_xx(sigma, p, last_swap_i)
+    sigma_xy = sigma[:, :, 0]
+    sigma_yy = sigma[:, :, 1]
+
+    mask = nu < 0.1
+    sigma_xx = np.ma.masked_where(mask, sigma_xx)
+    sigma_xy = np.ma.masked_where(mask, sigma_xy)
+    sigma_yy = np.ma.masked_where(mask, sigma_yy)
 
     im = ax[0, i].pcolormesh(
         x,
         y,
-        1e-3 * pressure.T,
-        # 1e-3 * sigma[:, :, 1].T,
+        # 1e-3 * pressure.T,
+        1e-3 * sigma_xx.T,
         cmap=cmap,
         vmin=0,
+        vmax=4,
         # vmax=max_angle,
         rasterized=True,
     )
-    cb = plt.colorbar(im, aspect=10)
-    cb.set_label(r"$p$ (kPa)", labelpad=labelpad)
+    if i == 1:
+        cb = plt.colorbar(im, aspect=10, ticks=[0, 4])
+        cb.set_label(r"$\sigma_{xx}$ (kPa)", labelpad=labelpad)
 
     im = ax[1, i].pcolormesh(
         x,
         y,
-        1e-3 * deviatoric.T,
+        # 1e-3 * deviatoric.T,
+        1e-3 * sigma_yy.T,
         cmap=cmap,
         vmin=0,
+        vmax=4,
         # vmax=max_angle,
         rasterized=True,
     )
-    cb = plt.colorbar(im, aspect=10)
-    cb.set_label(r"$\tau$ (kPa)", labelpad=labelpad)
+    if i == 1:
+        cb = plt.colorbar(im, aspect=10, ticks=[0, 4])
+        cb.set_label(r"$\sigma_{yy}$ (kPa)", labelpad=labelpad)
 
     im = ax[2, i].pcolormesh(
         x,
         y,
-        friction_angle.T,
-        cmap="Spectral",
-        vmin=p.repose_angle - 5,
-        vmax=p.repose_angle + 5,
+        1e-3 * sigma_xy.T,
+        # cmap=cmap,
+        vmin=-1.5,
+        vmax=1.5,
+        # friction_angle.T,
+        cmap="bwr",
+        # vmin=p.repose_angle - 5,
+        # vmax=p.repose_angle + 5,
         rasterized=True,
     )
-    cb = plt.colorbar(
-        im,
-        aspect=10,
-    )
-    cb.set_label(r"$\phi_\mathrm{mob}$ (°)", labelpad=labelpad)
+    if i == 1:
+        cb = plt.colorbar(
+            im,
+            aspect=10,
+            ticks=[-1.5, 0, 1.5],
+        )
+        cb.set_label(r"$\sigma_{xy}$ (kPa)", labelpad=labelpad)
     # cb.set_ticks([0, p.repose_angle, 2 * p.repose_angle])
-    cb.set_ticks([p.repose_angle - 5, p.repose_angle, p.repose_angle + 5])
+    # cb.set_ticks([p.repose_angle - 5, p.repose_angle, p.repose_angle + 5])
 
 
 for j in [0, 1]:
@@ -111,10 +130,10 @@ for j in [0, 1]:
 
 plt.sca(ax[-1, 0])
 plt.xlabel("$x$ (m)", labelpad=0)
-plt.ylabel("$y$ (m)", labelpad=0)  # , rotation="horizontal")  # ,labelpad=3)
+plt.ylabel("$y$ (m)", labelpad=-4)  # , rotation="horizontal")  # ,labelpad=3)
 plt.xticks([-W / 2, 0, W / 2])
 plt.yticks([0, p.H])
 
 
-plt.subplots_adjust(left=0.11, bottom=0.18, right=0.9, top=0.97, hspace=0.4, wspace=0.4)
+# plt.subplots_adjust(left=0.05, bottom=0.18, right=0.86, top=0.97, hspace=0.25, wspace=0.05)
 plt.savefig(os.path.expanduser("~/Dropbox/Apps/Overleaf/Kinematic_SLM/im/collapse_stress.pdf"))

papers/Kinematic_SLM/post_process/silo_alpha.py
@@ -49,7 +49,7 @@ for i, val in enumerate(alphas):
     dt = int(p.t_f / p.dt / 4)
     # dt = 500
 
-    for j, t in enumerate(range(nt_plot - dt, nt_plot + dt)):
+    for j, t in enumerate(range(nt_plot - dt, nt_plot + dt, p.save_inc)):
         this_u = np.load(f"output/silo_alpha/alpha_{val}/data/u_{str(t).zfill(6)}.npy")
         this_v = np.load(f"output/silo_alpha/alpha_{val}/data/v_{str(t).zfill(6)}.npy")
 

void_migration/motion/d2q4_array_v2.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 from numpy.typing import ArrayLike
 from void_migration import operators
@@ -56,8 +57,10 @@ def move_voids(
     s_bar = operators.get_average(s)
     S_bar = np.repeat(s_bar[:, :, np.newaxis], p.nm, axis=2)
 
-    potential_free_surface = operators.empty_up(nu, p)
-    # potential_free_surface = None
+    if p.slope_stability_model == "stress":
+        potential_free_surface = None
+    else:
+        potential_free_surface = operators.empty_up(nu, p)
 
     swap_indices = []
     dest_indices = []
@@ -83,19 +86,11 @@ def move_voids(
             s_inv_bar = operators.get_hyperbolic_average(s)
             S_inv_bar = np.repeat(s_inv_bar[:, :, np.newaxis], p.nm, axis=2)
             S_inv_bar_dest = np.roll(S_inv_bar, d, axis=axis)
-            # P = p.P_u_ref * (S_inv_bar_dest / dest)
+
             P = (p.dt / p.dy) * U_dest * (S_inv_bar_dest / dest)
-            # print(P[~np.isnan(P)].max())
 
             P[:, -1, :] = 0  # no swapping up from top row
         elif axis == 0:  # horizontal
-            # if p.advection_model == "average_size":
-            #     D = p.alpha * U_dest * S_bar_dest
-            #     P = D * (p.dt / p.dy**2) * (dest / S_bar_dest)
-            # elif p.advection_model == "freefall":
-            #     P = (
-            #         p.alpha * (p.dt / p.dy / p.dy) * U_dest * (dest / S_bar_dest)
-            #     )  # alpha has units of m, and is equal to the the variance (std dev^2) of the plume in the silo at y=0.5m
             P = p.alpha * U_dest * dest * (p.dt / p.dy / p.dy) * P_diff_weighting
 
             if d > 0:  # left
@@ -103,9 +98,14 @@ def move_voids(
             else:  # right
                 P[d:, :, :] = 0  # no swapping right from rightmost column
 
-            # slope_stable = operators.stable_slope_fast(s, d, p, potential_free_surface)
-            # slope_stable = operators.stable_slope_gradient(s, d, p)
-            slope_stable = operators.stable_slope_stress(s, p, last_swap)
+            if p.slope_stability_model == "gradient":
+                slope_stable = operators.stable_slope_fast(s, d, p, potential_free_surface)
+            elif p.slope_stability_model == "stress":
+                slope_stable = operators.stable_slope_stress(s, p, last_swap)
+            else:
+                sys.exit(f"Invalid slope stability model: {p.slope_stability_model}")
+
+            # Prevent swaps OUT FROM stable slope cells
             P[slope_stable] = 0
 
         swap_possible = unstable * ~np.isnan(dest)
@@ -116,10 +116,16 @@ def move_voids(
         this_dest_indices = this_swap_indices.copy()
         this_dest_indices[:, axis] -= d
 
+        # Prevent swaps INTO stable slope cells - this shouldnt be necessary
+        # if axis == 0:
+        #     stable_dest = slope_stable[this_dest_indices[:,0], this_dest_indices[:,1], this_dest_indices[:,2]]
+        #     this_swap_indices = this_swap_indices[~stable_dest,:]
+        #     this_dest_indices = this_dest_indices[~stable_dest,:]
+
         swap_indices.extend(this_swap_indices)
         dest_indices.extend(this_dest_indices)
 
-    # Prevent conflicts by filtering out swaps that would cause conflicts
+    # Prevent conflicts by filtering out swaps that would cause two voids to swap into the same cell
     swap_indices_conflict_free, dest_indices_conflict_free = prevent_conflicts(swap_indices, dest_indices)
 
     if len(swap_indices_conflict_free) > 0:

void_migration/operators.py
@@ -101,7 +101,11 @@ def stable_slope_stress(s, p, last_swap):
     sigma = stress.calculate_stress(s, last_swap, p)
     friction_angle = stress.get_friction_angle(sigma, p, last_swap)
 
-    stable = friction_angle <= p.repose_angle
+    stable = friction_angle <= (p.repose_angle + 2.0)
+    # print('aaaaa')
+    # print(friction_angle)
+    # print(p.repose_angle)
+    # print(stable)
 
     Stable = np.repeat(stable[:, :, np.newaxis], s.shape[2], axis=2)
     return Stable

void_migration/stress.py
@@ -41,6 +41,9 @@ def calculate_stress_fraction(last_swap, p):
     if p.stress_mode == "K_0":
         stress_fraction = np.sin(np.radians(p.repose_angle))
         stress_fraction = np.full([p.nx, p.ny], stress_fraction)
+    elif p.stress_mode == "no_lateral":
+        print("WARNING: Using no lateral stress")
+        stress_fraction = np.ones([p.nx, p.ny])
     elif p.stress_mode == "isotropic":
         stress_fraction = np.zeros([p.nx, p.ny])
     elif p.stress_mode == "active":
@@ -52,15 +55,28 @@ def calculate_stress_fraction(last_swap, p):
     elif p.stress_mode == "anisotropic":
         with warnings.catch_warnings():
             warnings.simplefilter("ignore", category=RuntimeWarning)
-            a = np.nanmean(last_swap, axis=2)  # 1 for up, -1 for left or right, 0 for isotropic
-            a_scaled = np.abs((a + 1) / 2)  # between 0 and 1, 0 for isotropic, 1 for fully anisotropic
+            a = np.nanmean(
+                np.abs(last_swap), axis=2
+            )  # between 0 and 1, 0 for isotropic, 1 for fully anisotropic
 
             K_a = (1 - np.sin(np.radians(p.repose_angle))) / (1 + np.sin(np.radians(p.repose_angle)))
-            # K = K_p * (K_a / K_p) ** ((a + 1) / 2)
-            K_iso = 1
+            K_iso = 1.0
             # K = K_iso * (K_a / K_iso) ** a_scaled
-            K = (K_a - K_iso) * a_scaled + 1
-            stress_fraction = np.full([p.nx, p.ny], 1 - K)
+            K = (K_a - K_iso) * a + K_iso
+            stress_fraction = 1 - K
+            # import matplotlib.pyplot as plt
+            # plt.figure(77)
+            # plt.ion()
+            # plt.clf()
+            # plt.subplot(121)
+            # plt.imshow(a.T)
+            # plt.colorbar()
+
+            # plt.subplot(122)
+            # plt.imshow(K.T)
+            # plt.colorbar()
+            # plt.pause(0.01)
+
     else:
         raise ValueError("Unknown stress mode")
 
@@ -68,8 +84,8 @@ def calculate_stress_fraction(last_swap, p):
 
 
 def calculate_stress(s, last_swap, p):
-    return calculate_stress_NEW(s, last_swap, p)
-    # return calculate_stress_OLD(s, last_swap, p)
+    # return calculate_stress_NEW(s, last_swap, p)
+    return calculate_stress_OLD(s, last_swap, p)
 
 
 def calculate_stress_OLD(s, last_swap, p):
@@ -151,8 +167,8 @@ def calculate_stress_NEW(s, last_swap, p):
                 else:
                     lr_frac = 1.0 / num_lr  # 0.5 if both, 1 if one
 
-                lr_frac = 0.5
-                p_total = 1.0
+                # lr_frac = 0.5
+                # p_total = 1.0
 
                 if p_total > 0:
                     with np.errstate(divide="ignore", invalid="ignore"):
@@ -214,10 +230,16 @@ def get_deviatoric(sigma, p, last_swap=None):
 
 
 def get_friction_angle(sigma, p, last_swap=None):
-    pressure = get_pressure(sigma, p, last_swap)
-    deviatoric = get_deviatoric(sigma, p, last_swap)
+    # pressure = get_pressure(sigma, p, last_swap)
+    # deviatoric = get_deviatoric(sigma, p, last_swap)
+    sigma_xy = sigma[:, :, 0]
+    sigma_yy = sigma[:, :, 1]
+    sigma_xx = get_sigma_xx(sigma, p, last_swap)
+
+    sigma_m = (sigma_xx + sigma_yy) / 2.0
+    sigma_d = np.sqrt(((sigma_yy - sigma_xx) / 2) ** 2 + sigma_xy**2)
 
     with np.errstate(divide="ignore", invalid="ignore"):
-        friction_angle = np.degrees(np.arcsin(deviatoric / pressure))
+        friction_angle = np.degrees(np.arcsin(sigma_d / sigma_m))
 
     return friction_angle